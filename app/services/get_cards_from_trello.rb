975e23164d5dbfca5757e47442a3fcd432a77e02
Romain Garcia <romain.garcia@gadz.org>
2020-03-10 17:25:58 +0100

    Rest client used and get cards from trello started

--- app/services/get_cards_from_trello.rb
+++ app/services/get_cards_from_trello.rb
@@ -1,21 +1,31 @@
-require 'open-uri'
-require 'net/http'
-require 'openssl'
+require 'rest-client'
 require 'json'
 
+class GetCardsFromTrello #< ApplicationService
 
+  def initialize(board_id)
+    @board_id = board_id
+  end
 
+  def self.call
+    params = {
+        cards: 'all',
+        card_fields: 'all', #id,name,dateLastActivity,desc,idLabels,labels
+        filter: 'open',
+        fields: 'all',
+        # key: ENV['TRELLO_API_KEY'],
+        key: '170be5e01f018dae9aba1a228c8a23b2',
+        # token: user.token
+        token: '83d92ded92b18476188d164979237e52b42db24c912d6d2b12be67f3098c4f68'
+      }
+    board_id = '5e5d2468afb9ce59727e2540'
+    response = RestClient.get "https://api.trello.com/1/boards/#{board_id}/lists", params: params
+    p response
 
-def get_lists_and_cards(board_id)
-  url = URI("https://api.trello.com/1/boards/#{board_id}/lists?cards=all&card_fields=all&filter=open&fields=all&key=#{TRELLO_API_KEY}&token=#{TRELLO_TOKEN}")
-
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  request = Net::HTTP::Get.new(url)
-
-  response = http.request(request)
-  # board_lists = JSON.parse(response.read_body).map {|list| list["name"]}
-  board_lists = JSON.parse(response.read_body)
+    # board_lists = JSON.parse(response.body).map {|list| list["name"]}
+    board_lists = JSON.parse(response.body)[0]['cards']
+    board_lists.each { |card| p card['name']}
+  end
 end
+
+GetCardsFromTrello.call
--- app/services/initialize_new_board.rb
+++ app/services/initialize_new_board.rb
@@ -1,75 +1,68 @@
-# require 'uri'
-require 'open-uri'
-require 'net/http'
-require 'openssl'
-require 'json'
+class CreateBoard < ApplicationService
 
-# TRELLO_API_KEY = see .env
-# TRELLO_TOKEN   = see .env
+  def initialize(user, board_name)
+    @user = user
+    @board_name = board_name
+  end
 
+  def create_board(user, board_name)
+    params = {
+      name: board_name,
+      defaultLabels: 'true',
+      defaultLists: 'false',
+      keepFromSource: 'none',
+      powerUps: 'all',
+      prefs_permissionLevel: 'private',
+      prefs_voting: 'disabled',
+      prefs_comments: 'members',
+      prefs_invitations: 'members',
+      prefs_cardCovers: 'true',
+      prefs_background: 'blue',
+      prefs_cardAging: 'regular',
+      key: ENV['TRELLO_API_KEY'],
+      token: user.token
+    }
 
-def create_board(board_name)
-  url = URI("https://api.trello.com/1/boards/?name=#{board_name}&defaultLabels=true&defaultLists=false&keepFromSource=none&powerUps=all&prefs_permissionLevel=private&prefs_voting=disabled&prefs_comments=members&prefs_invitations=members&prefs_selfJoin=true&prefs_cardCovers=true&prefs_background=blue&prefs_cardAging=regular&key=#{TRELLO_API_KEY}&token=#{TRELLO_TOKEN}")
+    response = RestClient.post "https://api.trello.com/1/boards", params
+    JSON.parse(response.body)["id"]
+  end
 
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  def create_lists(board_id, list_name = "")
+    list_names = ["Done", "To deploy", "Waiting for client", "To review/debug", "In Progress", "To Do", "Sprint2", "Sprint1"]
 
-  request = Net::HTTP::Post.new(url)
+    list_names.each do |list_name|
+      params = {
+        name: list_name,
+        pos: top,
+        key: ENV['TRELLO_API_KEY'],
+        token: user.token
+      }
 
-  response = http.request(request)
-  response.read_body
-  JSON.parse(response.read_body)["id"]
+      response = RestClient.post "https://api.trello.com/1/boards/#{board_id}/lists", params
+      # puts response.body
+    end
+  end
 
-end
+  def create_labels(board_id)
+    label_names = {"Sprint2": "yellow", "Sprint1": "green"}
 
-def create_lists(board_id, list_name = "")
-  list_names = ["Done", "To deploy", "Waiting for client", "To review/debug", "In Progress", "To Do", "Sprint2", "Sprint1"]
+    label_names.each do |label_name, label_color|
+      params = {
+        name: label_name,
+        color: label_color,
+        key: ENV['TRELLO_API_KEY'],
+        token: user.token
+      }
 
-  list_names.each do |list_name|
-    url = URI("https://api.trello.com/1/boards/#{board_id}/lists?name=#{list_name}&pos=top&key=#{TRELLO_API_KEY}&token=#{TRELLO_TOKEN}")
+      response = RestClient.post "https://api.trello.com/1/boards/#{board_id}/labels", params
+      # puts response.read_body
+    end
+  end
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(url)
-
-    response = http.request(request)
-    # puts response.read_body
+  def call
+    # board_id = create_board("@board_name")
+    board_id = create_board("my_new_board")
+    create_labels(board_id)
+    create_lists(board_id)
   end
 end
-
-def create_labels(board_id)
-  label_names = {"Sprint2" => "yellow", "Sprint1" => "green"}
-
-  label_names.each do |label_name, label_color|
-    p label_name
-    p label_color
-    url = URI("https://api.trello.com/1/boards/#{board_id}/labels?name=#{label_name}&color=#{label_color}&key=#{TRELLO_API_KEY}&token=#{TRELLO_TOKEN}")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(url)
-
-    response = http.request(request)
-    puts response.read_body
-  end
-end
-
-
-
-
-
-
-
-
-# p get_lists(BOARD_ID)
-
-board_id = create_board("my_new_board_2")
-create_labels(board_id)
-
-create_lists(board_id)
-
