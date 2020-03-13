7c51c5cc7a7bfe5fac9114fc04100e558cf59fc6
youenn priser <youenn.priser@gmail.com>
2020-03-13 15:47:48 +0100

    Tried debug js - failed

--- app/controllers/trello_webhooks_controller.rb
+++ app/controllers/trello_webhooks_controller.rb
@@ -36,7 +36,7 @@ class TrelloWebhooksController < ActionController::Base
     if list_name == 'to do'
       # task.new(user_story_id: , weight: , title: , current_status: 'To do', trello_card_id: )
       Task.create!(title: card_name, current_status: 'to do', trello_card_id: card_id)
-    elsif list_name.split(' ').include?('Sprint')
+    elsif list_name.split(' ').include?('sprint')
       # UserStory.create(sprint_id: , done: false, title: ,trello_card_id: , trello_card_short_link:)
       sprint = Sprint.find_by(trello_list_id: list_id)
       UserStory.create(sprint: sprint, title: card_name ,trello_card_id: card_id, trello_card_short_link: short_link)
