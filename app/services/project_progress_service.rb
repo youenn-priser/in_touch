b9139c797c37cce0a8a1e5b5c881a9a6cc9480c8
Romain Garcia <romain.garcia@gadz.org>
2020-03-15 17:55:06 +0100

    updated service to fix dynamic progress bar

--- app/services/project_progress_service.rb
+++ app/services/project_progress_service.rb
@@ -1,14 +1,13 @@
 class ProjectProgressService
   def initialize(project)
-    @project = project
+    @tasks = project.tasks
   end
 
   def call
     #moyenne % d'avancement du projet general= (nbr de user story done * le poids) sur / (la somme du nombre de user story * le poids)
-    tasks = @project.reload.tasks
-    tasks_weight_one   = tasks.where(weight: 1).count * 1 || 0
-    tasks_weight_three = tasks.where(weight: 3).count * 3 || 0
-    tasks_weight_five  = tasks.where(weight: 5).count * 5 || 0
+    tasks_weight_one   = @tasks.where(weight: 1).count * 1
+    tasks_weight_three = @tasks.where(weight: 3).count * 3
+    tasks_weight_five  = @tasks.where(weight: 5).count * 5
 
     weight_total = tasks_weight_one + tasks_weight_three + tasks_weight_five
 
@@ -17,10 +16,10 @@ class ProjectProgressService
     # multiplierr le count de chaque array par le poids
     # additiopnner ces poids
 
-    tasks_done = tasks.where(current_status: "done")
-    tasks_done_weight_one   = tasks_done.where(weight: 1).count * 1 || 0
-    tasks_done_weight_three = tasks_done.where(weight: 3).count * 3 || 0
-    tasks_done_weight_five  = tasks_done.where(weight: 5).count * 5 || 0
+    tasks_done = @tasks.where(current_status: "done")
+    tasks_done_weight_one   = tasks_done.where(weight: 1).count * 1
+    tasks_done_weight_three = tasks_done.where(weight: 3).count * 3
+    tasks_done_weight_five  = tasks_done.where(weight: 5).count * 5
 
     weight_task_done_total = tasks_done_weight_one + tasks_done_weight_three + tasks_done_weight_five
     # calculer poids des tasks done
