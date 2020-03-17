a404374a05bad9d9c6305ee64977d6bf955ac296
Romain Garcia <romain.garcia@gadz.org>
2020-03-17 17:45:25 +0100

    fixed reports controller with modules and srvices

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -14,7 +14,7 @@ class ReportsController < ApplicationController
     @project           = Project.find(project_params[:project_id])
     @report            = Report.new(client_email: report_params[:report_client_email], description: report_params[:report_description], report_topic: report_params[:report_topic])
     @report.project_id = project_params[:project_id]
-    @report.actual_db_record = RecordModule::DbToJsonService.new(@project)
+    @report.actual_db_record = RecordModule::DbToJsonService.new(@project).call
     if @report.save
       mail = ReportMailer.with(project: @project.id, subject: @report.report_topic ,content: @report.description).report
       mail.deliver_now
