4b3941d525ec48723598131f38055827d8b78c90
Romain Garcia <romain.garcia@gadz.org>
2020-03-17 15:56:46 +0100

    add tasks to sprints

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,6 +1,7 @@
 class Sprint < ApplicationRecord
   belongs_to :project
   has_many :user_stories
+  has_many :tasks, through: :user_stories
 
   validates :title, presence: true
   validates :trello_list_id, uniqueness: true, allow_nil: true
