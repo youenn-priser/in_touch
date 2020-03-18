bb6146f6f32757531a33607fddab0e563856ab21
Thibault Comte <comte.thibault@icloud.com>
2020-03-18 10:17:25 +0100

    redirect when sign in and sign out done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # 'http://localhost:3000/projects'
+    projects_path
+  end
+
+  def after_sign_out_path_for(resource)
+    # 'http://localhost:3000/'
+    root_path
+  end
+
 end
