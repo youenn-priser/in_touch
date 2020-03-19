6e834a22655fee2cc651705ec30e951d310dff67
youenn priser <youenn.priser@gmail.com>
2020-03-19 11:52:38 +0100

    Added the active behavior on recap cards in the recap history

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 import "bootstrap";
 import { initCircleBar } from '../components/circle_bar';
 initCircleBar();
+import { activeReportCards } from '../components/report_cards_active';
+activeReportCards();
 import { show_client_form } from '../new_project_form';
 show_client_form();
-import { ActiveUsCards } from '../components/us-cards-active';
-ActiveUsCards()
+import { activeUsCards } from '../components/us_cards_active';
+activeUsCards();
