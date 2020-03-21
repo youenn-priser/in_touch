09ce1f61ac3fb47e2979d558366a9ce73f10b37d
youenn priser <youenn.priser@gmail.com>
2020-03-21 13:06:34 +0100

    From single-user to multiple-user separtating users projects and clients

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :validatable,
-
+  has_many :projects
   devise :omniauthable, omniauth_providers: %i[trello]
 
     def self.find_for_trello_oauth(auth)
